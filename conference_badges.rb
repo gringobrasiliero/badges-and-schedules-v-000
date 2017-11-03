5216b5352c1ccaf9a8cdb89bbcf4386ed8663962
Nolan Byrnes <nolanehs@gmail.com>
2017-11-03 19:56:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+badges = []
+attendees.each do |name|
+  badges.push("Hello, my name is #{name}.")
+end
+return badges
+end
+
+def assign_rooms(attendees)
+room_assignments= []
+room = 1
+attendees.each do |name|
+  room_assignments.push("Hello, #{name}! You'll be assigned to room #{room}! ")
+  room+= 1
+end
+return room_assignments
+end
+
+
+
+end
