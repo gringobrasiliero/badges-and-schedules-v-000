99e628ce7446cff2b0dfac4f336936c142a343d2
Nolan Byrnes <nolanehs@gmail.com>
2017-11-03 19:57:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(attendees)
 room_assignments= []
 room = 1
 attendees.each do |name|
-  room_assignments.push("Hello, #{name}! You'll be assigned to room #{room}! ")
+  room_assignments.push("Hello, #{name}! You'll be assigned to room #{room}!")
   room+= 1
 end
 return room_assignments
